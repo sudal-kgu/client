2102b9b1ddf9e8705cd6cc04252fa29e5e9283ff
youmjieun <162525149+youmjieun@users.noreply.github.com>
2026-02-01 22:56:32 +0900

    feat: 초기 설정

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-green-600">
+      <h1 className="text-white text-4xl font-black" />
+    </div>
+  )
+}
+export default App
