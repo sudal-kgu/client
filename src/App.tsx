af45e705d87f57278f752078edb48be7538ab0e4
sonyk9919 <sonyk9919@sonyk9919ui-MacBookAir.local>
2026-02-06 12:43:54 +0900

    chore: 변경된 개행 방식 적용
    - npm run format

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import './App.css';
 
 const queryClient = new QueryClient({
