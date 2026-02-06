451759210c6084a483a3abf696f90e3e27b54fcd
sonyk9919 <sonyk9919@sonyk9919ui-MacBookAir.local>
2026-02-06 12:43:54 +0900

    chore: 변경된 개행 방식 적용
    - npm run format

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
-import globals from 'globals';
+import prettier from 'eslint-config-prettier';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
 import tseslint from 'typescript-eslint';
-import prettier from 'eslint-config-prettier';
 
 export default [
     { ignores: ['dist'] },
