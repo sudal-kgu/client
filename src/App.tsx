39b8fa980e265375acbc3367322a0a6a823c6ea8
youmjieun <162525149+youmjieun@users.noreply.github.com>
2026-03-03 22:10:28 +0900

    fix: 라우팅 오류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RouterProvider } from 'react-router-dom';
 
-import './App.css';
 import { router } from './router';
 
 const queryClient = new QueryClient({
@@ -15,13 +14,15 @@ const queryClient = new QueryClient({
     },
 });
 
-function App() {
+export default function App() {
     return (
         <QueryClientProvider client={queryClient}>
-            <RouterProvider router={router} />
+            <div className="min-h-screen bg-[#0e1412] text-white">
+                <div className="mx-auto w-full px-6 py-6">
+                    <RouterProvider router={router} />
+                </div>
+            </div>
             <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
     );
 }
-
-export default App;
--- src/router.tsx
+++ src/router.tsx
@@ -1,17 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom';
 
-import App from './App';
 import TrashDetail from './pages/TrashDetail.tsx';
 import TrashResult from './pages/TrashResult.tsx';
 
 export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App />,
-        children: [
-            { index: true, element: <TrashResult /> },
-            { path: 'result', element: <TrashResult /> },
-            { path: 'detail/:id', element: <TrashDetail /> },
-        ],
-    },
+    { path: '/', element: <TrashResult /> },
+    { path: '/result', element: <TrashResult /> },
+    { path: '/detail/:id', element: <TrashDetail /> },
 ]);
