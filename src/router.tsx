dfb5e23bdb0a9314418f5339ba70cf44cff7065c
youmjieun <162525149+youmjieun@users.noreply.github.com>
2026-04-12 00:22:10 +0900

    fix:router 구조 수정

--- src/router.tsx
+++ src/router.tsx
@@ -1,32 +1,56 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { Outlet, createBrowserRouter } from 'react-router-dom';
 
 import Basket from './components/camera/Basket';
+import MeProvider from './components/provider/MeProvider';
 import Checklist from './pages/Checklist';
+import KakaoRedirect from './pages/KakaoRedirect';
+import Login from './pages/Login';
 import TrashAnalysisResult from './pages/TrashAnalysisResult';
 import TrashCamera from './pages/TrashCamera';
 import TrashDetail from './pages/TrashDetail';
 
 export const router = createBrowserRouter([
     {
-        path: '/camera',
-        element: <TrashCamera />,
+        element: (
+            <MeProvider>
+                <Outlet />
+            </MeProvider>
+        ),
         children: [
             {
-                path: 'basket',
-                element: <Basket />,
+                path: '/',
+                element: <></>,
+            },
+            {
+                path: '/login',
+                element: <Login />,
+            },
+            {
+                path: '/kakao/redirect',
+                element: <KakaoRedirect />,
+            },
+            {
+                path: '/camera',
+                element: <TrashCamera />,
+                children: [
+                    {
+                        path: 'basket',
+                        element: <Basket />,
+                    },
+                ],
+            },
+            {
+                path: '/analysis/:id',
+                element: <TrashAnalysisResult />,
+            },
+            {
+                path: '/analysis/:analysisId/trashes/:trashId',
+                element: <TrashDetail />,
+            },
+            {
+                path: '/checklist/:trashId',
+                element: <Checklist />,
             },
         ],
-    },
-    {
-        path: '/analysis/:id',
-        element: <TrashAnalysisResult />,
-    },
-    {
-        path: '/analysis/:analysisId/trashes/:trashId',
-        element: <TrashDetail />,
-    },
-    {
-        path: '/checklist/:trashId',
-        element: <Checklist />,
     },
 ]);
