dfcb050f36d03214d24ffac25c198ab57a6cff8f
youmjieun <162525149+youmjieun@users.noreply.github.com>
2026-04-22 01:17:52 +0900

    fix:라우터 임시 수정

--- src/router.tsx
+++ src/router.tsx
@@ -3,23 +3,21 @@ import { Outlet, createBrowserRouter } from 'react-router-dom';
 import Basket from './components/camera/Basket';
 import MeProvider from './components/provider/MeProvider';
 import Checklist from './pages/Checklist';
+import Game from './pages/Game';
 import KakaoRedirect from './pages/KakaoRedirect';
 import Login from './pages/Login';
+import PointShop from './pages/PointShop';
 import TrashAnalysisResult from './pages/TrashAnalysisResult';
 import TrashCamera from './pages/TrashCamera';
 import TrashDetail from './pages/TrashDetail';
 
 export const router = createBrowserRouter([
     {
-        element: (
-            <MeProvider>
-                <Outlet />
-            </MeProvider>
-        ),
+        element: <Outlet />,
         children: [
             {
                 path: '/',
-                element: <></>,
+                element: <Game />,
             },
             {
                 path: '/login',
@@ -48,8 +46,16 @@ export const router = createBrowserRouter([
                 element: <TrashDetail />,
             },
             {
-                path: '/checklist/:trashId',
+                path: '/checklist/:analysisId/:trashId',
                 element: <Checklist />,
+            },
+            {
+                path: '/game',
+                element: <Game />,
+            },
+            {
+                path: '/point-shop',
+                element: <PointShop />,
             },
         ],
     },
